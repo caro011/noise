1a4c45b88f7ed395ab00eb2e07addcfe3ec1215a
noise-lkd <lekhaduy2811@gmail.com>
2026-07-09 13:01:44 +0700

    update website

--- components/ui/FadeIn.tsx
+++ components/ui/FadeIn.tsx
@@ -1,21 +1,13 @@
 "use client";
 
-import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
 type FadeInProps = {
   children: ReactNode;
 };
 
-export default function FadeIn({ children }: FadeInProps) {
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 60 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.2 }}
-      transition={{ duration: 0.8 }}
-    >
-      {children}
-    </motion.div>
-  );
+export default function FadeIn({
+  children,
+}: FadeInProps) {
+  return <>{children}</>;
 }
